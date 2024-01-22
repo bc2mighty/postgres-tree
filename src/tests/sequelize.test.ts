fe5070015aaf185c268c76e43bbd9707b2550b3a
bc2mighty <akinadetunmise8@gmail.com>
2024-01-22 01:54:08 +0100

    feat: tests

--- src/tests/sequelize.test.ts
+++ src/tests/sequelize.test.ts
@@ -1,11 +1,11 @@
-import { createCategory } from "../db/queries";
+import { createNewCategory } from "../db/queries";
 const createCategoryMock = jest.fn()
 
 describe('Query Mocks', () => {
     test('should create a category', async() => {
         const label = '1', labelPath = '1', fullPath = '1';
         createCategoryMock.mockReturnValue([]);
-        const result = await createCategory(label, labelPath, fullPath);
+        const result = await createNewCategory(label, labelPath, fullPath);
         expect(result).toBe([])
     })
 })
